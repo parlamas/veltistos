b302547a23c8af5bb31bf2119bb58989a3c9b22c
parlamas <parlamas@live.com>
2025-10-07 11:55:02 +0200

    8000a

--- src/content/home.ts
+++ src/content/home.ts
@@ -41,6 +41,7 @@ export const homeSlots: HomeSlots = {
       title: "Τά παραθετικά τού <span class='wow'>ἀγαθός</span>",
       img: "", // OK to leave blank if your component handles it
       kicker: "Γραμματική",
+      number: "αριθμός 2 • number 2",
     },
   ],
 
@@ -50,7 +51,8 @@ export const homeSlots: HomeSlots = {
       title:
         "Ήρθε η ώρα η Ελλάδα και η Κύπρος να ενωθούν!<br>It is high time Greece and Cyprus united!",
       img: "/union/enosis-01.jpg",
-      excerpt: "Τό 1ο άρθρο.",
+      excerpt: "Οι διαδικασίες αρχίζουν αμέσως...",
+      number: "αριθμός 1 • number 1",
     },
   ],
 

8b3c67ac1a89743ce634881a11fadf271035d215
parlamas <parlamas@live.com>
2025-10-09 09:50:36 +0200

    135

--- src/app/diethni/nobel-irony/page.tsx
+++ src/app/diethni/nobel-irony/page.tsx
@@ -92,7 +92,7 @@ export default function Page() {
               </span>
             </h1>
             <p className="text-sm text-zinc-500">
-  <span hidden>α4 • a4</span><br />
+  <span hidden>α4 • a4</span></p>
             </div>
         </header>
 

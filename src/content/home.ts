a318953aea6d3554b181e6ce474183a93fefdebc
parlamas <parlamas@live.com>
2025-10-11 12:03:13 +0200

    AAA007

--- src/content/home.ts
+++ src/content/home.ts
@@ -7,6 +7,8 @@ export type LeftItem = {
   img: string;
   kicker: string;
   number?: string;
+  date?: string;
+  author?: string;
 };
 
 export type LeadItem = {
@@ -15,6 +17,8 @@ export type LeadItem = {
   img: string;
   excerpt: string;
   number?: string;
+  date?: string;
+  author?: string;
 };
 
 export type RightItem = {
@@ -25,6 +29,8 @@ export type RightItem = {
   width?: number;   // optional display hint
   height?: number;  // optional display hint
   number?: string;
+  date?: string;
+  author?: string;
 };
 
 // Combined slots type
@@ -38,42 +44,52 @@ export type HomeSlots = {
 export const homeSlots: HomeSlots = {
   left: [
     {
+      author: "Ισίδωρος Παρλαμάς<br />Isidoros Parlamas"
       href: "/stories/parathetika",
       title: "Τά παραθετικά τού <span class='wow'>ἀγαθός</span>",
       img: "",
       kicker: "Γραμματική",
+      date: "Οκτ./Oct. 3, 2025",
       number: '<span class="r">Αναζήτηση/Search:</span> αα2 ή/or aa2',
     },
     {
+      author: "Ισίδωρος Παρλαμάς<br />Isidoros Parlamas"
       href: "/diethni/nobel-irony",
       title: "Δώστε το Νομπελ Ειρήνης στον Νετανιάχου<br />Give the Nobel Peace prize to Netanyahu",
       img: "",
       kicker: "Διεθνή/International",
+      date: "Οκτ./Oct. 7, 2025",
       number: '<span class="r">Αναζήτηση/Search:</span> αα4 ή/or aa4',
     },
   ],
 
   middle: [
     {
+      author: "Ισίδωρος Παρλαμάς<br />Isidoros Parlamas"
       href: "/stories/union",
       title: "Ήρθε η ώρα η Ελλάδα και η Κύπρος να ενωθούν!<br />It is high time Greece and Cyprus united!",
       img: "/union/enosis-01.jpg",
       excerpt: "Οι διαδικασίες αρχίζουν αμέσως...",
+      date: "Οκτ./Oct. 1, 2025",
       number: '<span class="r">Αναζήτηση/Search:</span> αα1 ή/or aa1',
     },
   ],
 
   right: [
     {
+      author: "Ισίδωρος Παρλαμάς<br />Isidoros Parlamas"
       href: "/stories/parts-of-speech",
       title: "Τα μέρη του λόγου.",
       kicker: "Γραμματική",
+      date: "Οκτ./Oct. 5, 2025",
       number: '<span class="r">Αναζήτηση/Search:</span> αα3 ή/or aa3',
     },
     {
+      author: "Ισίδωρος Παρλαμάς<br />Isidoros Parlamas"
       href: "/stories/republic/b1-001",
       title: "ἡ Πολιτεία • Βιβλίο 1/1<br />The Republic • Book 1/1",
       kicker: "Γλώσσες/Languages",
+      date: "Οκτ./Oct. 9, 2025",
       number: '<span class="r">Αναζήτηση/Search:</span> αα5 ή/or aa5',
     },
   ],
